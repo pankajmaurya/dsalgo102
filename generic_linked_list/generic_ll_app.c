10408f9d57ba33ebecd5c46fcde10407b421d792
Pankaj Maurya <pankajmaurya@gmail.com>
2025-01-15 22:07:05 +0530

    Added some logic to have a linked list of x fruits and then do removal

--- generic_linked_list/generic_ll_app.c
+++ generic_linked_list/generic_ll_app.c
@@ -1,16 +1,23 @@
+#include <string.h> // for strcmp()
 #include <stdio.h>
 #include <stdlib.h>
 #include "lib.h"
 
 typedef struct appdata {
 	int x;
+	char* fruit;
 } AppData;
 
 int compare_demo(void* a, void* b) {
 	AppData* da = (AppData*) a;
 	AppData* db = (AppData*) b;
 	printf("Comparing values: %d and %d\n", da->x, db->x);
-	return (da->x - db->x);
+	int fruitCmp = strcmp(da->fruit, db->fruit);
+	if (fruitCmp == 0) {
+		return (da->x - db->x);
+	} else {
+		return fruitCmp;
+	}
 }
 
 int main(int argc, char **argv) {
@@ -19,17 +26,19 @@ int main(int argc, char **argv) {
 	init(ll, fun_ptr);
     printf("Linked list size : %d\n", ll->size);
 
-	for (int i = 1; i <= 10; i++) {
+	for (int i = 1; i <= 6; i++) {
 		AppData* d = malloc(sizeof(AppData));
 		d->x = i;
+		d->fruit = "apple";
 		add(ll, d);
 	}
         printf("Linked list size : %d\n", ll->size);
         
 	// Now remove the node with payload = d
-	for (int i = 6; i < 14; i++) {
+	for (int i = 3; i <= 6; i++) {
 		AppData* d = malloc(sizeof(AppData));
 		d->x = i;
+		d->fruit = "apple";
 		remove_ll_cmp(ll, d);
 	}
         printf("Linked list size : %d\n", ll->size);
